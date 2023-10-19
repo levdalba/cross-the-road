561ed42fd6e787cc05202936512b626c3142b4fa
levdalba <levanidalbashvili00@gmail.com>
2023-10-19 09:41:03 +0700

    random condition added

--- car_manager.py
+++ car_manager.py
@@ -1,18 +1,19 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-import random
-from turtle import Turtle
 
 
 class CarManager:
     def __init__(self):
         self.all_cars = []
         self.car_speed = STARTING_MOVE_DISTANCE
+        self.car_creation_chance = 1
 
     def create_car(self):
-        random_chance = random.randint(1, 6)
-        if random_chance == 1:
+        if random.randint(1, 6) == self.car_creation_chance:
             new_car = Turtle("square")
             new_car.shapesize(stretch_wid=1, stretch_len=2)
             new_car.color(random.choice(COLORS))
@@ -27,6 +28,7 @@ class CarManager:
 
     def level_up(self):
         self.car_speed += MOVE_INCREMENT
+        self.car_creation_chance += 1
 
     def collision(self, player):
         for car in self.all_cars:
